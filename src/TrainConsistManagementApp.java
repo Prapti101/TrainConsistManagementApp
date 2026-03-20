6c3d71aab93bce086937a15ae033a8d2c4da4213
Prapti101 <praptipriya486@gmail.com>
2026-03-20 14:49:52 +0530

    UC5:Preserve insertion order

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -2,21 +2,14 @@ import java.util.*;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        LinkedList<String> consist = new LinkedList<>();
+        LinkedHashSet<String> formation = new LinkedHashSet<>();
 
-        consist.add("Engine");
-        consist.add("Sleeper");
-        consist.add("AC");
-        consist.add("Cargo");
-        consist.add("Guard");
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper");
 
-        consist.add(2, "Pantry");
-
-        consist.removeFirst();
-        consist.removeLast();
-
-        for (String bogie : consist) {
-            System.out.println(bogie);
-        }
+        System.out.println(formation);
     }
 }
