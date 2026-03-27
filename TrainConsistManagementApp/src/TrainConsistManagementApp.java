2697f045143ba9da037b0ecc82d69dabea6b141e
Prapti101 <praptipriya486@gmail.com>
2026-03-27 14:41:10 +0530

    Added UC7 Java files properly

---
+++ TrainConsistManagementApp/src/TrainConsistManagementApp.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
+public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        List<Bogie> list = new ArrayList<>();
+
+        list.add(new Bogie("Sleeper", 72));
+        list.add(new Bogie("AC Chair", 56));
+        list.add(new Bogie("First Class", 40));
+
+        list.sort(Comparator.comparingInt(b -> b.capacity));
+
+        for (Bogie b : list) {
+            System.out.println(b.name + " : " + b.capacity);
+        }
+    }
+}
